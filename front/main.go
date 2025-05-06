4de771f323261aac2ecb5872fa0de7112747cfcf
Cein <slyva.dima@gmail.com>
2025-05-06 04:23:29 +0200

    minor changes

--- front/main.go
+++ front/main.go
@@ -6,18 +6,13 @@ import (
 )
 
 func main() {
-	// Define the port for the frontend server
-	frontendPort := "8080" // You can change this port if needed
-
-	// Create a file server handler pointing to the current directory (".")
+	frontendPort := "8080" 
 	fs := http.FileServer(http.Dir("."))
 
-	// Handle all requests using the file server
 	http.Handle("/", fs)
 
 	log.Printf("Frontend server listening on port %s\n", frontendPort)
 
-	// Start the server
 	err := http.ListenAndServe(":"+frontendPort, nil)
 	if err != nil {
 		log.Fatalf("Frontend server failed to start: %v", err)
